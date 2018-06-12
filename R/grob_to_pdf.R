f32f354cca0a34d6e8713048e64b3bd79190efff
Calvin Floyd <calvinmfloyd@yahoo.com>
2018-06-11 22:46:27 -0400

    edits to grob_to_pdf()

--- R/grob_to_pdf.R
+++ R/grob_to_pdf.R
@@ -24,7 +24,11 @@ grob_to_pdf <- function(..., file_name = character()){
     width = mm_to_in(sum(as.numeric(grob_list[[1]]$widths))))
 
   for(gll in grob_list){
-    grid.arrange(gll)
+    if(!is.grob(gll)){
+      closed <- grDevices::dev.off()
+      stop('All inputs must be grobs.')
+    }
+    gridExtra::grid.arrange(gll)
   }
 
   closed <- grDevices::dev.off()
